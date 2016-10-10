c0c832ba5a2f3f2d3a630e19224c264b7fde5722
xutl <79109681@qq.com>
2016-10-10 15:26:20 +0800

    c

--- DatabaseQueue.php
+++ DatabaseQueue.php
@@ -6,11 +6,45 @@
  */
 namespace xutl\queue;
 
+/**
+ * Class DatabaseQueue
+ * @package xutl\queue
+ */
 class DatabaseQueue extends Queue
 {
+    /**
+     * @var \yii\db\Connection|string Default database connection component name
+     */
+    public $connection = 'db';
+
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         parent::init();
+        if (is_string($this->connection)) {
+            $this->connection = Yii::$app->get($this->connection);
+        } elseif (is_array($this->connection)) {
+            if (!isset($this->connection['class'])) {
+                $this->connection['class'] = Connection::className();
+            }
+            $this->connection = Yii::createObject($this->connection);
+        }
+        if (!$this->connection instanceof Connection) {
+            throw new InvalidConfigException("Queue::connection must be application component ID of a SQL connection.");
+        }
+    }
+
+    /**
+     * 推送一个任务到队列
+     * @param mixed $payload
+     * @param null $queue
+     * @param int $delay
+     */
+    public function push($payload, $queue = null, $delay = 0)
+    {
+        // TODO: Implement push() method.
     }
 
     /**
